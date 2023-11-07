60503ab9d515c9144fe7c5d5c14cd3799e4d2864
Juan <juan.parra@smartsoft.com.co>
2023-11-06 21:56:30 -0500

    Restructurando plantilla original

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola a todos este es un nuevo proyecto</Text>
+    </View>
+  );
+};
+
+export default App;
